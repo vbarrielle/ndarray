3c75ddb85d8616f3adce1d07812d00a1d821449c
root <root@localhost>
2014-08-15 00:04:38 +0200

    Update docs

---
+++ doc/implementors/core/ops/trait.Shr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ndarray'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;A: <a class='trait' href='http://doc.rust-lang.org/master/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> + <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Shr.html' title='core::ops::Shr'>Shr</a>&lt;A, A&gt;, D: <a class='trait' href='ndarray/trait.Dimension.html' title='ndarray::Dimension'>Dimension</a>, E: <a class='trait' href='ndarray/trait.Dimension.html' title='ndarray::Dimension'>Dimension</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Shr.html' title='core::ops::Shr'>Shr</a>&lt;<a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, E&gt;, <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, D&gt;&gt; for <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, D&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
