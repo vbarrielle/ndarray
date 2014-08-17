555cfddad01f055a4e8cdc400684c9080bb77819
root <root@localhost>
2014-08-17 17:57:20 +0200

    Update docs

---
+++ doc/implementors/core/iter/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ndarray'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;&amp;'a A&gt; for <a class='struct' href='ndarray/struct.Elements.html' title='ndarray::Elements'>Elements</a>&lt;'a, A, <a class='type' href='ndarray/type.Ix.html' title='ndarray::Ix'>Ix</a>&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;&amp;'a mut A&gt; for <a class='struct' href='ndarray/struct.ElementsMut.html' title='ndarray::ElementsMut'>ElementsMut</a>&lt;'a, A, <a class='type' href='ndarray/type.Ix.html' title='ndarray::Ix'>Ix</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/iter/trait.ExactSize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ndarray'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.ExactSize.html' title='core::iter::ExactSize'>ExactSize</a>&lt;&amp;'a A&gt; for <a class='struct' href='ndarray/struct.Elements.html' title='ndarray::Elements'>Elements</a>&lt;'a, A, <a class='type' href='ndarray/type.Ix.html' title='ndarray::Ix'>Ix</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
