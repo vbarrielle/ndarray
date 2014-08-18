5399c3e997b03c00554fdaee9fe5aadec7dd1e3e
root <root@localhost>
2014-08-18 20:57:48 +0200

    Update docs

--- doc/implementors/core/iter/trait.FromIterator.js
+++ doc/implementors/core/iter/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['ndarray'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;A: <a class='trait' href='http://doc.rust-lang.org/master/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, <a href='http://doc.rust-lang.org/master/std/primitive.uint.html'>uint</a>&gt;",];
+implementors['ndarray'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;A&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, <a class='type' href='ndarray/type.Ix.html' title='ndarray::Ix'>Ix</a>&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
