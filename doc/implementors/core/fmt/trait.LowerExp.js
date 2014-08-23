bde1e8e29e5feea5e9998dd23f4959fd9c3a6d78
root <root@localhost>
2014-08-23 17:17:05 +0200

    Update docs

---
+++ doc/implementors/core/fmt/trait.LowerExp.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ndarray'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a>, D: <a class='trait' href='ndarray/trait.Dimension.html' title='ndarray::Dimension'>Dimension</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, D&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
